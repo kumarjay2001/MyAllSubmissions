c2794cc1a51efd2f02557ce86be6593821365312
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-03-20 21:45:34 +0530

    Time: 19 ms (42.54%), Space: 43.5 MB (28.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
+// ..............  Method 1.............
+ /*
         s= s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int low=0;
 		int high=s.length()-1;
@@ -11,5 +13,15 @@ class Solution {
 		    }
 		}
 	  return true;
+*/
+        
+        
+// ..............  Method 2.............   
+       s= s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        StringBuilder sb=new StringBuilder();
+        sb.append(s).reverse();
+        return s.equals(sb.toString());
+        
+
     }
 }
