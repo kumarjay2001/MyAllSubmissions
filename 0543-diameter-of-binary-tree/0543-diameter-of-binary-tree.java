8f7897da8ec8a0c66962213ef703a951a54db465
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-03-28 20:15:40 +0530

    Time: 1 ms (25.40%), Space: 42 MB (75.49%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,20 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
+        
+//         It's a first bruth approach having O(N^2) comp.....
+        /*
+    public int diameterOfBinaryTree(TreeNode root) {    
         if(root==null)return 0;
         int lh=height(root.left);
         int rh=height(root.right);
@@ -27,4 +16,27 @@ class Solution {
         if(root==null)return 0;
         return (Math.max(height(root.left),height(root.right))+1);
     }
+    */ 
+    class Info{
+        int diam;
+        int ht;
+        public Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+   public int diameterOfBinaryTree(TreeNode root) {
+       return diamm(root).diam-1;
+   }
+    public Info diamm(TreeNode root){
+        if(root==null){
+            return new Info(0,0);
+        }
+        Info leftInfo=diamm(root.left);
+        Info rightInfo=diamm(root.right);
+        int diam=Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
+        int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
+        return new Info(diam,ht);
+    }    
+    
 }
