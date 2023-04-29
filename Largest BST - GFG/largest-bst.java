04f7174d4460d86770bb7449f64ca4cedb6c77af
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-04-29 17:42:46 +0530

    Added solution - LeetHub

--- Largest BST - GFG/largest-bst.java
+++ Largest BST - GFG/largest-bst.java
@@ -97,6 +97,7 @@ class GFG
 // } Driver Code Ends
 
 
+
 //User function Template for Java
 
 // class Node  
@@ -110,46 +111,35 @@ class GFG
 //         left = right = null; 
 //     } 
 // }
-class NodeValue {
-    public int maxNode, minNode, maxSize;
-    
-    NodeValue(int minNode, int maxNode, int maxSize) {
-        this.maxNode = maxNode;
-        this.minNode = minNode;
-        this.maxSize = maxSize;
+class Info{
+        int min;int max;int size;
+       Info(int min,int max,int size){
+            this.min=min;
+            this.max=max;
+            this.size=size;
+        }
     }
-};
 
 class Solution{
     
-
+    // Return the size of the largest sub-tree which is also a BST
+    
     static int largestBst(Node root)
     {
-               return largestBSTSubtreeHelper(root).maxSize;
-
+     return large(root).size;
         
     }
-     private static NodeValue largestBSTSubtreeHelper(Node root) {
-        // An empty tree is a BST of size 0.
-        if (root == null) {
-            return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
+    static Info large(Node root){
+        if(root==null){
+            return new Info(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
         }
-        
-        // Get values from left and right subtree of current tree.
-        NodeValue left = largestBSTSubtreeHelper(root.left);
-        NodeValue right = largestBSTSubtreeHelper(root.right);
-        
-        // Current node is greater than max in left AND smaller than min in right, it is a BST.
-        if (left.maxNode < root.data && root.data < right.minNode) {
-            // It is a BST.
-            return new NodeValue(Math.min(root.data, left.minNode), Math.max(root.data, right.maxNode), 
-                                left.maxSize + right.maxSize + 1);
-        }
-        
-        // Otherwise, return [-inf, inf] so that parent can't be valid BST
-        return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, 
-                            Math.max(left.maxSize, right.maxSize));
+         Info ltree=large(root.left);
+         Info rtree=large(root.right);
+         if(root.data>ltree.max && root.data<rtree.min){
+             return new Info(Math.min(ltree.min,root.data),Math.max(root.data,rtree.max),ltree.size+rtree.size+1);
+         }
+         
+         return new Info(Integer.MIN_VALUE,Integer.MAX_VALUE,Math.max(ltree.size,rtree.size));
     }
- 
     
 }
