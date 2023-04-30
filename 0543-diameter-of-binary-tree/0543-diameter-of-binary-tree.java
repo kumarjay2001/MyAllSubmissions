c96ccc223d2f0bfa4afe03197c20a3c9bd74a15e
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-04-30 11:38:32 +0530

    Time: 14 ms (6.30%), Space: 42.4 MB (55.96%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,42 +1,28 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-        
-//         It's a first bruth approach having O(N^2) comp.....
-        /*
-    public int diameterOfBinaryTree(TreeNode root) {    
+    public int diameterOfBinaryTree(TreeNode root) {
         if(root==null)return 0;
         int lh=height(root.left);
         int rh=height(root.right);
-        int ldi=diameterOfBinaryTree(root.left);
-        int rdi=diameterOfBinaryTree(root.right);
-        int orgiheight=lh+rh;
-        return Math.max(Math.max(ldi,rdi),orgiheight);
+        int diam=lh+rh;
+        return Math.max(diam,Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right)));
     }
     public int height(TreeNode root){
         if(root==null)return 0;
-        return (Math.max(height(root.left),height(root.right))+1);
+        return Math.max(height(root.left),height(root.right))+1;
     }
-    */ 
-    class Info{
-        int diam;
-        int ht;
-        public Info(int diam,int ht){
-            this.diam=diam;
-            this.ht=ht;
-        }
-    }
-   public int diameterOfBinaryTree(TreeNode root) {
-       return diamm(root).diam-1;
-   }
-    public Info diamm(TreeNode root){
-        if(root==null){
-            return new Info(0,0);
-        }
-        Info leftInfo=diamm(root.left);
-        Info rightInfo=diamm(root.right);
-        int diam=Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
-        int ht=Math.max(leftInfo.ht,rightInfo.ht)+1;
-        return new Info(diam,ht);
-    }    
-    
 }
