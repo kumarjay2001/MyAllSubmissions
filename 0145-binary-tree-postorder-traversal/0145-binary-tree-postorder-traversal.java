79f4a29fec1568855c3b52c3a2d8a00b995a939f
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-01 12:28:47 +0530

    Time: 1 ms (16.84%), Space: 40.4 MB (90.23%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,28 +15,45 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> al=new ArrayList<>();
-        Stack<TreeNode> s=new Stack<>();
-        TreeNode curr=root;
-        TreeNode temp=null;
-        while(curr!=null || !s.isEmpty()){
-            if(curr!=null){
-                s.push(curr);
-                curr=curr.left;
-            }else{
-                temp=s.peek().right;
-                if(temp==null){
-                    temp=s.pop();
-                    al.add(temp.val);
-                    while(!s.isEmpty() && temp==s.peek().right){
-                        temp=s.pop();
-                        al.add(temp.val);
-                    }
-                }else{
-                    curr=temp;
-                }
+//         ArrayList<Integer> al=new ArrayList<>();
+//         Stack<TreeNode> s=new Stack<>();
+//         TreeNode curr=root;
+//         TreeNode temp=null;
+//         while(curr!=null || !s.isEmpty()){
+//             if(curr!=null){
+//                 s.push(curr);
+//                 curr=curr.left;
+//             }else{
+//                 temp=s.peek().right;
+//                 if(temp==null){
+//                     temp=s.pop();
+//                     al.add(temp.val);
+//                     while(!s.isEmpty() && temp==s.peek().right){
+//                         temp=s.pop();
+//                         al.add(temp.val);
+//                     }
+//                 }else{
+//                     curr=temp;
+//                 }
                 
-            }
+//             }
+//         }
+//         return al;
+        
+        ArrayList<Integer> al=new ArrayList<>();
+        if(root==null)return al;
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        s1.push(root);
+        
+        while(!s1.isEmpty()){
+            root=s1.pop();
+            s2.push(root);
+            if(root.left!=null)s1.push(root.left);
+            if(root.right!=null)s1.push(root.right);
+        }
+        while(!s2.isEmpty()){
+            al.add(s2.pop().val);
         }
         return al;
     }
