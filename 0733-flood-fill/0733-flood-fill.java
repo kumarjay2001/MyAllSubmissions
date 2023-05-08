a49212ebbb0aff94a4660f3881b10b048e0c0bfe
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-08 18:02:04 +0530

    Time: 1 ms (43.67%), Space: 43.8 MB (8.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int m=image.length;
         int n=image[0].length;
@@ -25,4 +26,44 @@ class Solution {
             dfs(image,target,i,j-1,color);
         }
     }
+    
+    */
+    
+//      BFS
+    class Pair{
+        int sr;int sc;
+            Pair(int sr,int sc){
+            this.sr=sr;this.sc=sc;
+        }
+    }
+     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+         int target=image[sr][sc];
+         image[sr][sc]=color;
+         if(target==color){
+             return image;
+         }
+         final int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+         Queue<Pair> q=new LinkedList<>();
+         q.add(new Pair(sr,sc));
+         while(!q.isEmpty()){
+             Pair curr=q.remove();
+             for(int []dir:directions){
+                 int nrow=curr.sr+dir[0];
+                 int ncol=curr.sc+dir[1];
+                 if(isValid(nrow,ncol,image,target)){
+                     image[nrow][ncol]=color;
+                     q.add(new Pair(nrow,ncol));
+                 }
+             }
+         }
+         return image;
+     }
+    public boolean isValid(int nrow,int ncol,int [][]image,int target){
+        if(nrow<0 || nrow>=image.length || ncol<0 || ncol>=image[0].length || image[nrow][ncol]!=target){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
 }
