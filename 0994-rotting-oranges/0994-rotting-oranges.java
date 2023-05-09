71d722a9c14aefc052ae7e37a5831f3f8c9bf304
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-09 13:20:34 +0530

    Time: 2 ms (85.14%), Space: 42 MB (74.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,55 +1,54 @@
 class Solution {
-     int res=0;
     class Pair{
-        int r;int c;int t;
-        Pair(int r,int c,int t){
-            this.r=r;
-            this.c=c;
-            this.t=t; 
+        int i;int j;int t;
+        Pair(int i,int j,int t){
+            this.i=i;
+            this.j=j;
+            this.t=t;
         }
     }
     public int orangesRotting(int[][] grid) {
         int m=grid.length;
-        int mycount=0;
         int n=grid[0].length;
-        int [][]vis=new int[m][n];
         Queue<Pair> q=new LinkedList<>();
+        int tnt=0;
+        int [][] vis=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
                     vis[i][j]=2;
+                    q.add(new Pair(i,j,0));
                 }else if(grid[i][j]==0){
                     vis[i][j]=0;
                 }else{
-                    mycount++;
+                    tnt++;
                 }
             }
         }
-        int tm=0;
-        int cnt=0;
-        int []delr={-1,0,1,0};
-        int []delc={0,1,0,-1};
+        
+        int []fi={-1,0,1, 0};
+        int []si={0 ,1,0,-1};
+        int time=0;
+        int cnte=0;
         while(!q.isEmpty()){
-            int r=q.peek().r;
-            int c=q.peek().c;
+            int r=q.peek().i;
+            int c=q.peek().j;
             int t=q.peek().t;
-            tm=Math.max(tm,t);
+            time=Math.max(time,t);
             q.remove();
             for(int i=0;i<4;i++){
-                int nr=r+delr[i];
-                int nc=c+delc[i];
-                if(nr>=0&&nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && vis[nr][nc]!=2){
+                int nr=r+fi[i];
+                int nc=c+si[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n &&vis[nr][nc]!=2 && grid[nr][nc]==1){
+                    grid[nr][nc]=2;
+                    cnte++;
                     q.add(new Pair(nr,nc,t+1));
-                    vis[nr][nc]=2;
-                    cnt++;
                 }
             }
         }
-        if(cnt!=mycount){
+        if(cnte!=tnt){
             return -1;
         }
-        return tm;
+        return time;
     }
-   
 }
