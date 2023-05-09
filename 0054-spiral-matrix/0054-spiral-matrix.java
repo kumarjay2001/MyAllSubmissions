72c0919dcc89b5b413d6ad76cfa81fc2eca9a219
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-09 12:59:48 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (69.94%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-       ArrayList<Integer> al=new ArrayList<>();
+        List<Integer> al=new ArrayList<>();
         int m=matrix.length;
         int n=matrix[0].length;
-        int startrow=0,startcol=0,endrow=m-1,endcol=n-1;
-        while(startrow<=endrow && startcol<=endcol){
-            for(int i=startcol;i<=endcol;i++){
-                 System.out.println("Huehue");
-                al.add(matrix[startrow][i]);
+        int strow=0,stcol=0,endrow=m-1,endcol=n-1;
+        while(strow<=endrow && stcol<=endcol){
+            for(int i=stcol;i<=endcol;i++){
+                al.add(matrix[strow][i]);
             }
-            for(int j=startrow+1;j<=endrow;j++){
-               
+            for(int j=strow+1;j<=endrow;j++){
                 al.add(matrix[j][endcol]);
             }
-            for(int i=endcol-1;i>=startcol;i--){
-                 if(startrow==endrow){ // if there is a single row
-                    return al;
-                }
-                al.add(matrix[endrow][i]);
+            for(int i=endcol-1;i>=stcol;i--){
+                 if(strow==endrow){
+                     break;
+                 }
+                 al.add(matrix[endrow][i]);
             }
-            for(int j=endrow-1;j>=startrow+1;j--){
-                 if(startcol==endcol){ // if there is a single col
-                    return al;
+            for(int i=endrow-1;i>=strow+1;i--){
+                if(stcol==endcol){
+                    break;
                 }
-                al.add(matrix[j][startcol]);
+                 al.add(matrix[i][stcol]);
             }
-            startrow++;endrow--;startcol++;endcol--;
+            strow++;stcol++;endrow--;endcol--;
         }
         return al;
     }
