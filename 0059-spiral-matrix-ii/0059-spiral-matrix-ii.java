6cac24f3f9f8be2f72fa029e628c9eb10ecdcd2a
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-10 10:10:24 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (17.46%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int [][]matrix=new int[n][n];
-        int strow=0,stcol=0,endcol=n-1,endrow=n-1;
-        int content=1;
-    while(strow<=endrow && stcol<=endcol){
-            
+        int stcol=0,strow=0,endrow=n-1,endcol=n-1;
+        int [][]renew=new int[n][n];
+        int reso=1;
+      while(stcol<=endcol && strow<=endrow){
         for(int i=stcol;i<=endcol;i++){
-                matrix[strow][i]=content;
-                content++;
+            renew[strow][i]=reso++;
         }
-        
         for(int j=strow+1;j<=endrow;j++){
-            matrix[j][endcol]=content;
-            content++;
+            renew[j][endcol]=reso++;
         }
         for(int i=endcol-1;i>=stcol;i--){
-            matrix[endrow][i]=content;
-            content++;
+            if(strow==endrow){
+                break;
+            }
+            renew[endrow][i]=reso++;
         }
-        for(int j=endrow-1;j>=strow+1;j--){
-            matrix[j][stcol]=content;
-            content++;
-        } 
-        strow++;stcol++;endrow--;endcol--;
-      }
-       
-        return matrix;
+        for(int j=endrow-1;j>=stcol+1;j--){
+            if(stcol==endcol){
+                break;
+            }
+            renew[j][stcol]=reso++;
+        }
+        stcol++;endcol--;strow++;endrow--;
+      }    
+        return renew;
     }
 }
