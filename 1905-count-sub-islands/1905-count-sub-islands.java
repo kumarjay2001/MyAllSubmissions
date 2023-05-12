0e7d321165bb8ab60f723e128d8bb8f877dbf39a
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-12 12:07:53 +0530

    Time: 25 ms (67.26%), Space: 72 MB (67.75%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,34 +1,36 @@
 class Solution {
-   int ans =0;
-public void dfs(int[][] grid1, int[][] grid2, int i , int j){
-    if(i<0|| i>=grid1.length ||j<0|| j>=grid1[0].length || grid2[i][j]==0 ) {
-        return;
-    }
-    if(grid1[i][j]== 0)ans =0;
-    grid2[i][j] =0;
-    dfs(grid1, grid2, i-1, j);
-    dfs(grid1, grid2, i+1, j);
-    dfs(grid1, grid2, i, j+1);
-    dfs(grid1, grid2, i, j-1);
-}
-
-
-
-public int countSubIslands(int[][] grid1, int[][] grid2) {
-    // problem can be solved using dfs
+    boolean ans=false;
     int res=0;
-    for(int i=0 ;i <grid1.length ;i++){
-        for(int j=0 ;j<grid2[0].length ;j++){
-            
-            if(grid2[i][j]==1)
-            {
-                ans=1;
-                dfs(grid1, grid2, i, j);
-                res+=ans;
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m=grid1.length;
+        int n=grid1[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid2[i][j]==1){
+                    ans=true;
+                    dfs(i,j,grid1,grid2);
+                    if(ans==true){
+                        res++;
+                    }
+                    
+                }
             }
         }
+        return res;
     }
-    return res;
+    public void dfs(int i,int j,int [][]grid1,int [][]grid2){
+        if(i<0|| i>=grid1.length ||j<0|| j>=grid1[0].length || grid2[i][j]==0 ) {
+           return;
+        }
+        if(grid1[i][j]==0){
+            ans=false;
+        }
+        grid2[i][j]=0;
+        dfs(i-1,j,grid1,grid2);
+        dfs(i+1,j,grid1,grid2);
+        dfs(i,j-1,grid1,grid2);
+        dfs(i,j+1,grid1,grid2);
+        
+    }
     
-}
 }
