3dbd209f673e58d9aa64c65fb4e0ce224e4bfa1b
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-05-17 10:53:44 +0530

    Time: 5 ms (89.63%), Space: 62.2 MB (57.30%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -13,18 +13,18 @@ class Solution {
         ListNode slow=head;
         ListNode fast=head;
         ListNode prev=null;
-        while(fast!=null && fast.next!=null ){
+        while(fast!=null && fast.next!=null){
             prev=slow;
             slow=slow.next;
             fast=fast.next.next;
         }
-        ListNode head2=slow;
+        ListNode headme=slow;
         prev.next=null;
-        head2=reverse(head2);
-       // return head2.val;
+        ListNode head2=reverse(headme);
+        
         int maxo=0;
         while(head!=null && head2!=null){
-            maxo=Math.max(maxo,head.val+head2.val);
+            maxo=Math.max(head.val+head2.val , maxo);
             head=head.next;
             head2=head2.next;
         }
@@ -40,7 +40,6 @@ class Solution {
             prev=curr;
             curr=next;
         }
-        return prev;
-        
+         return prev;   
     }
 }
