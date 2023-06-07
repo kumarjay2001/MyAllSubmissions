c1b042bf9454f666c77dec41ebd41e589eabdafa
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-06-07 23:35:28 +0530

    Time: 12 ms (89.77%), Space: 49.6 MB (40.70%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -2,11 +2,9 @@ class Solution {
     class Pair{
         int node;
         int dis;
-        // int wt;
         Pair(int node,int dis){
             this.node=node;
             this.dis=dis;
-            // this.wt=wt;
         }
     }
     public int countPaths(int n, int[][] roads) {
@@ -17,35 +15,29 @@ class Solution {
 			 adj.get(roads[i][1]).add(new Pair(roads[i][0], roads[i][2]));
 		}
         int []dist=new int[n];
-        int []ways=new int[n];
-        
-         Arrays.fill(ways , 0);
         int mod=(int) 1e9+7;
+        int []ways=new int[n];
+        ways[0]=1;
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[0]=0;
-        ways[0]=1;
-        PriorityQueue<Pair>pq=new PriorityQueue<>((x,y)-> x.dis-y.dis);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.dis-b.dis);
         pq.add(new Pair(0,0));
         while(!pq.isEmpty()){
             Pair it=pq.remove();
             int node=it.node;
             int dis=it.dis;
-            
             for(Pair p:adj.get(node)){
                 int adjnode=p.node;
                 int nw=p.dis;
-                
                 if(nw+dis<dist[adjnode]){
                     dist[adjnode]=nw+dis;
                     pq.add(new Pair(adjnode,dist[adjnode]));
-                    ways[adjnode]=ways[node]  ;
+                    ways[adjnode]=ways[node];
+                }else if(nw+dis==dist[adjnode]){
+                    ways[adjnode]=(ways[adjnode]+ways[node]) % mod;
                 }
-                else if(nw+dis==dist[adjnode]){
-                     ways[adjnode]=(ways[adjnode]+ways[node]) % mod; 
-                 }    
-            }    
+            }
         }
         return ways[n - 1] % mod;
-
     }
 }
