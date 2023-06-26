6ab8df62cb483e20fc6d69bb713fda8d943cc6e9
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-06-26 14:45:32 +0530

    Time: 65 ms (55.58%), Space: 58.1 MB (35.65%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -1,19 +1,16 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
-        int i = 0;
-        int j = costs.length - 1;
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-
-        long ans = 0;
-        while (k-- > 0) {
-            while (pq1.size() < candidates && i <= j) {
+       PriorityQueue<Integer> pq1=new PriorityQueue<>();
+       PriorityQueue<Integer> pq2=new PriorityQueue<>();
+        long ans=0;
+        int i=0,j=costs.length-1;
+        while(k-->0){
+            while(pq1.size()<candidates && i<=j){
                 pq1.offer(costs[i++]);
             }
-            while (pq2.size() < candidates && i <= j) {
+            while(pq2.size()<candidates && i<=j){
                 pq2.offer(costs[j--]);
             }
-
             int t1 = pq1.size() > 0 ? pq1.peek() : Integer.MAX_VALUE;
             int t2 = pq2.size() > 0 ? pq2.peek() : Integer.MAX_VALUE;
 
@@ -24,6 +21,7 @@ class Solution {
                 ans += t2;
                 pq2.poll();
             }
+            
         }
         return ans;
     }
