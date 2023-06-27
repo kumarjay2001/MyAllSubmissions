54643ddc992460892d787c187b7d8695674281f8
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-06-28 00:47:46 +0530

    Time: 5 ms (81.39%), Space: 65.5 MB (49.08%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,53 +1,59 @@
+class DisjointSet{
+    int []size;
+    int []par;
+    public DisjointSet(int n){
+        size=new int[n];
+        par=new int[n];
+        for(int i=0;i<n;i++){
+            size[i]=1;
+            par[i]=i;
+        }
+    }
+    public int parent(int u){
+        if(u==par[u]){
+            return u;
+        }
+        return par[u]=parent(par[u]);
+    }
+    public void union(int u,int v){
+       int paru=parent(u);
+        int parv=parent(v);
+        if(paru==parv)return ;
+        if(size[paru]<size[parv]){
+            par[paru]=parv;
+            size[parv]+=size[paru];
+        }
+        else{
+            par[parv]=paru;
+            size[paru]+=size[parv];
+        }
+    }
+}
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        int m=connections.length;
-        int ni=connections[0].length;
-        if(n-1>m)return -1;
-       List<List<Integer>> al=new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            al.add(new ArrayList<>());
+        int V=connections.length;
+        int exnd=0;
+        DisjointSet ds=new DisjointSet(n);
+        for(int i=0;i<V;i++){
+            // for(int j=0;j<V;j++){
+                int u=connections[i][0];
+                int v=connections[i][1];
+                if(ds.parent(u)==ds.parent(v)){
+                    exnd++;
+                }else{
+                    ds.union(u,v);
+                }
+            // }
         }
-        int res=0;
-        for(int k[]:connections){
-            al.get(k[0]).add(k[1]);
-            al.get(k[1]).add(k[0]);
-        }
-        int []vis=new int[n];
+        int cmp=0;
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                res++;
-                dfs(i,al,vis);
-            }
+           if(ds.par[i]==i){
+               cmp++;
+           } 
         }
-        return res-1;
-    }
-    public void dfs(int i,List<List<Integer>> al,int []vis){
-        vis[i]=1;
-        for(Integer j:al.get(i)){
-            if(vis[j]==0){
-                dfs(j,al,vis);
-            }
+        if(exnd>=cmp-1){
+            return cmp-1;
         }
+        return -1;
     }
-    // public int makeConnected(int n, int[][] c) {
-    //     if(c.length<n-1) return -1;
-    //     List<List<Integer>> ans = new ArrayList<>();
-    //     for(int i=0;i<n+1;i++) ans.add(new ArrayList<>());
-    //     for(int k[]:c){
-    //         ans.get(k[0]).add(k[1]);
-    //         ans.get(k[1]).add(k[0]);
-    //     }
-    //     int res=0, vis[] = new int[n];
-    //     for(int i=0;i<n;i++)
-    //         if(vis[i]==0){
-    //             res++;
-    //             dfs(i,ans,vis);
-    //         }
-    //     return res-1;
-    // }
-    // void dfs(int node,List<List<Integer>> ans,int vis[] ){
-    //     vis[node]=1;
-    //     for(int k:ans.get(node))
-    //         if(vis[k]==0) dfs(k,ans,vis);
-    // }
 }
