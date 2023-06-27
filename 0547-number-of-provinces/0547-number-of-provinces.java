c74d15b26fc8cd4f535d1a727a55f8cc7756ff66
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-06-27 23:36:08 +0530

    Time: 2 ms (40.16%), Space: 46.2 MB (6.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,34 +1,49 @@
+class DisjointSet{
+    int[]size;
+    int []par;
+    public DisjointSet(int n){
+        size=new int[n];
+        par=new int[n];
+        for(int i=0;i<n;i++){
+            size[i]=1;
+            par[i]=i;
+        }
+    }
+    public int parent(int u){
+        if(u==par[u])return u;
+        return par[u]=parent(par[u]);
+    }
+    public void union(int u,int v){
+        int paru=parent(u);
+        int parv=parent(v);
+        if(paru==parv)return ;
+        if(size[paru]<size[parv]){
+            par[paru]=parv;
+            size[parv]+=size[paru];
+        }
+        else{
+            par[parv]=paru;
+            size[paru]+=size[parv];
+        }
+    }
+}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> al=new ArrayList<>();
-        int V=isConnected.length;
-        for(int i=0;i<V;i++){
-            al.add(new ArrayList<>());
-        }
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++ ){
-                if(isConnected[i][j]==1 && i!=j){
-                    al.get(i).add(j);
-                    al.get(j).add(i);
+        int n=isConnected.length;
+        DisjointSet ds=new DisjointSet(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    ds.union(i,j);
                 }
             }
         }
-        int count=0;
-        boolean []vis=new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                count++;
-                dfs(al,i,vis);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(ds.par[i]==i){
+                cnt++;
             }
         }
-        return count;
-    }
-    public void dfs(ArrayList<ArrayList<Integer>> adj,int curr,boolean []vis){
-        vis[curr]=true;
-        for(Integer it:adj.get(curr)){
-            while(!vis[it]){
-                dfs(adj,it,vis);
-            }
-        }
+        return cnt;
     }
 }
