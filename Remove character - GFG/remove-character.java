f2fadfdb83048e78e63ff2c823fcd75bc8d94c8c
Jay Kumar <98164903+kumarjay2001@users.noreply.github.com>
2023-07-04 22:09:23 +0530

    Added solution - LeetHub

---
+++ Remove character - GFG/remove-character.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String string1 = read.readLine();
+            String string2 = read.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.removeChars(string1,string2) );
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String removeChars(String string1, String string2){
+        // code here
+        HashSet<Character> hs=new HashSet<>();
+        for(int i=0;i<string2.length();i++){
+            hs.add(string2.charAt(i));
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<string1.length();i++){
+            if(!hs.contains(string1.charAt(i))){
+                sb.append(string1.charAt(i));
+                
+            }
+        }
+        // for(int i=0;i<)
+        return sb.toString();
+    }
+}
